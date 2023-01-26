9a24aaabdc7aba02d6a7573b424fd3eb0d9b20e5
akshaylodhe <120580022+akshaylodhe@users.noreply.github.com>
2023-01-26 20:19:34 +0530

    function of scope

---
+++ javascript/variable.js
@@ -0,0 +1,57 @@
+
+//syntax:let/var/const variable_name:value
+// reserved_keywords variable_name :date 
+let b;//declration
+b = 30;// Defination
+
+//let a = 20//d+d
+//let a = 'Akshay'//redeclaration with let keyword is not possible
+
+var t;//Declaretion
+t=50; //Defination
+
+
+let favFruit = 'mango';
+console.log(favFruit);
+
+var favcar = 'mincup';//redeclration of variable with var keyboard is possible
+console.log(favcar);
+ 
+var favcar ='swift';
+console.log(favcar)
+
+mobno = 7038907370; // Define :hoisting of variable :first define variables, value then declare  variable 
+var mobno ;// declare  hoisting is not possible with lett keyword 
+console.log(mobno)
+
+const myname ='Akshay Patil'; //value resign with const keyworrds is not possible
+//myname=5000;
+console.log(myname);
+
+//g = 50;//hoisting is not possible
+//const g;
+
+
+
+// function syntax: function (reserved keyword) functionName(){}********
+
+function myFirstFunction(){
+    //code block
+    console.log('i am inside function');
+}
+
+
+myFirstFunction();
+console.log(myFirstFunction());
+
+
+function scope(){
+    let myFavMovies = '3iditos'
+  console.log( myFavMovies);
+}
+
+
+
+
+
+
