f8c2012478386d81a6410da286846ed500ddacdb
akshaylodhe <120580022+akshaylodhe@users.noreply.github.com>
2023-01-28 17:45:37 +0530

    console file

--- javascript/consoleTests.js
+++ javascript/consoleTests.js
@@ -1,1 +1,28 @@
-     
+//console.log(); test cases 
+
+//let msg = 'msg for mysurname variable ###--->'
+let mysurname = 'lodhe';
+let myfirstname = 'AKShay';
+console.log('mysurname', mysurname);
+console.log('myfirstname', myfirstname);
+
+let num1 = 20;
+console.log("num1, num1");
+console.log( myfirstname,mysurname,num1);
+console.log('msg concadition' ,  myfirstname + mysurname + num1);
+let num2 = 40;
+console.log(num1,num2);
+console.log(num1 + num2);
+
+console.log(num1 + mysurname + num1 );
+console.log(num2 , mysurname , num2 );
+
+console.log(num1 +myfirstname + num2 );
+console.log(num1 , myfirstname , num2 );
+console.log(num1 + num2 + myfirstname);
+
+
+console.log(num1+  " " + num2 +" " + myfirstname +num1 +num2 + num2+num2);
+
+console.log("num-->"+ num1);
+console.log("num-->", num1);
