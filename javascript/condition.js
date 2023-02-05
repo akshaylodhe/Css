3c71a97edd2c7e955ae22bbac133498ae92d65ce
akshaylodhe <120580022+akshaylodhe@users.noreply.github.com>
2023-02-05 11:13:51 +0530

    conditione

--- javascript/condition.js
+++ javascript/condition.js
@@ -1,12 +1,55 @@
 //if(condition){
-   // condition =='true'
+// cond  =='true'
 //}
 
 var number = 60;
 
-if(number < ) 
-console.log('number is greater');
+if (number < 30) {
+   console.log('number is greater');
+}
+
+else {
+   console.log('nunber is smaller');
+}
+
+if (number == 40) {
+   console.log(number);
+} else if (number == 60) {
+   console.log(number);
+} else if (number == 50) {
+   console.log(number);
+}
+else {
+   console.log('else block', number);
+}
+
+
+//  if (number == 40) {
+// console.log(number);
+// } if (number == 60) {
+//  console.log(number);
+//  } if ( number == 50) {
+//  console.log(number);
+//  }
+//else{
+//console.log(number);
+//}
 
 
 
+//switch-statament:
+switch (number) {
+   case 40:
+      console.log(number);
+      break;
+   case 50:
+      console.log(number);
+  
+   case 80:
+      console.log(number);
+      break;
 
+   default:
+      console.log('default');
+      break;
+}
