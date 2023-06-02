245589762c7323fa9957b833ac7517474298998e
akshaylodhe <120580022+akshaylodhe@users.noreply.github.com>
2023-06-02 08:58:09 +0530

    Adding file angular

--- javascript/Nov-Angular/angularpro/src/app/app.component.ts
+++ javascript/Nov-Angular/angularpro/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; // import section  : where we add dependancy classes
 
-@Component({
+@Component({ //component directive section : metadata about component 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angularpro';
+export class AppComponent { // main component class : where we write code logic 
+  title = 'angularpro'; // properties  
+  myName = "Akshay";
+  mysurname : string = "lodhe";
+  mysurname2? : string; //syntex change
+  //mysurname2 : string
+  constructor() { // constructor : just normal function but it wiill trigger implicty by complier
+    console.log('inside constructor..');
+  }
+
+test(){
+this.mysurname2 = 'Patil';
+}
+
+test2(){
+  this.mysurname2 = this.mysurname;
+  this.test();
+}
+
 }
